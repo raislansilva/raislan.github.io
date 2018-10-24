3ea73af922810dd955d81143350dd0aa54b319b4
raislan <raislan@live.com>
2018-10-23 21:13:53 -0300

    Adicionado logica do sorteio

--- js/script.js
+++ js/script.js
@@ -1,11 +1,102 @@
- 
-var p = document.getElementById('mega');	
+var loteria = 0
+var array = new Array()
+function ordernar(a, b){
+    return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
+}
 
- 
- 
-    
-    paragrafo.addEventListener('click',	atualizarNome);	
 
-    function atualizarNome(){	
+
+function geraAposta(jogo){
+    switch(jogo) {
+
+        case "mega":
+            while(array.length < 6){
+             var x = Math.floor((Math.random() * 60) + 1);
+             var exist = array.indexOf(x) >= 0;
+                if (!exist) {
+                    array.push(x);
+                }
+
+              loteria++
+            }
+
+            loteria = 0
+            array.sort(ordernar)
+
+            for(var i = 0; i < array.length;i++){
+                var resultado = array.toString().replace(/,/g, " "); 
+                document.getElementById("res").innerHTML = resultado
+            }
+            array = new Array()
+            break;
+
+        case "facil":
+
+            while(array.length < 15){
+                var x = Math.floor((Math.random() * 25) + 1);
+                var exist = array.indexOf(x) >= 0;
+                if (!exist) {
+                    array.push(x);
+                }
+
+                loteria++
+            }
+
+            loteria = 0
+            array.sort(ordernar)
+
+            for(var i = 0; i < array.length;i++){
+                var resultado = array.toString().replace(/,/g, " "); 
+                document.getElementById("res").innerHTML = resultado
+            }
+            array = new Array()
+            break;
+
+        case "quina":
+
+            while(array.length < 5){
+                var x = Math.floor((Math.random() * 80) + 1);
+                var exist = array.indexOf(x) >= 0;
+                if (!exist) {
+                    array.push(x);
+                }
+
+                loteria++
+            }
+
+            loteria = 0
+            array.sort(ordernar)
+
+            for(var i = 0; i < array.length;i++){
+                var resultado = array.toString().replace(/,/g, " "); 
+                document.getElementById("res").innerHTML = resultado
+            }
+            array = new Array()
+            break;
+
+       case "mania":
        
+            while(array.length < 50){
+                var x = Math.floor((Math.random() * 100) + 1);
+                var exist = array.indexOf(x) >= 0;
+                if (!exist) {
+                    array.push(x);
+                }
+
+                loteria++
+            }
+
+            loteria = 0
+            array.sort(ordernar)
+
+            for(var i = 0; i < array.length;i++){
+                var resultado = array.toString().replace(/,/g, " "); 
+                document.getElementById("res").innerHTML = resultado
+            }
+            array = new Array()
+            break; 
+
+        default:
+            console.log("JOGO NÃO EXISTE")
+    }
 }
